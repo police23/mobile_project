a2d0d5d0163533d38972f8745ed6643c47cdc209
police23 <22520088@gm.uit.edu.vn>
2025-03-16 15:25:25 +0700

    added music_player_layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,11 @@ android {
 
 dependencies {
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.material)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
